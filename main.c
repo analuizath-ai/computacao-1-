ba5c028a93b779e7b7a7b6941409dc7e94fcf2dd
analuizath-ai <analuizahomem@alunos.utfpr.edu.br>
2025-08-13 11:22:29 -0300

    IMC

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   printf("Calculo do IMC \n");
+   printf("Altura: %f \n" , 1.64);
+   printf("Peso: %f \n" , 62.5);
+   
+   printf("IMC: %f " , 62.5 / (1.64*1.64));
+   
+   return 0;
+}
+
