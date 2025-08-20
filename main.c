eb69fd820568002cb8bd99bff415864325a513b8
analuizath-ai <analuizahomem@alunos.utfpr.edu.br>
2025-08-20 10:32:01 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,18 @@
-/******************************************************************************
+ #include <stdio.h>
+ int main() {
+    float n1, n2, aux;
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  int codigo;
+ 
+ 
+  printf("Digite o primeiro valor:");
+  scanf ("%f", &n1);
 
-*******************************************************************************/
-#include <stdio.h>
-
-int main()
-{
-   printf("Calculo do IMC \n");
-   printf("Altura: %f \n" , 1.64);
-   printf("Peso: %f \n" , 62.5);
-   
-   printf("IMC: %f " , 62.5 / (1.64*1.64));
-   
-   return 0;
+  printf("Digite o segundo valor:");
+  scanf ("%f", &n2);
+ 
+  aux = n1 + n2;
+ 
+  printf("%f", aux);
+  return 0;
 }
-
