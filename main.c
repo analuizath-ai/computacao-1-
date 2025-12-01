426936df3a71366caf14c53aa1dc34a3f11a36ba
analuizath-ai <analuizahomem@alunos.utfpr.edu.br>
2025-12-01 20:46:20 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,83 @@
-  #include <stdio.h> 
-  
-  int main() {
-    float A, B, C;
-      
-    printf("Digite o valor do lado A: ");
-    scanf ("%f", &A);
-    printf("Digite o valor do lado B: ");
-    scanf ("%f", &B);
-    printf("Digite o valor do lado C: ");
-    scanf ("%f", &C);
-    
-      
-    if (A + B > C && A + C > B && B + C > A) {
-    printf("Os valores formam um triangulo.\n");
-      if (A == B && B == C) {
-    printf("Triangulo equiltero.\n");
-      } else if (A == B || B == C || A == C) {
-    printf("Triangulo isosceles.\n");
-      } else {
-    printf("Triangulo escaleno.\n"); 
-      }
-    } else {
-    printf("Os valores nao formam um triangulo.\n");
-    }
-    
-    return 0;
-    
-    }
-    
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int ehVogal(char c) {
+	c = tolower((unsigned char)c);
+	return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
+int ehConsoante(char c) {
+	c = tolower((unsigned char)c);
+	if (c >= 'a' && c <= 'z' && !ehVogal(c)) {
+		return 1;
+	}
+	return 0;
+}
+
+void inverteString(char s[]) {
+	int i = 0;
+	int j = (int)strlen(s) - 1;
+	while (i < j) {
+		char temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
+
+int main() {
+	char original[4][3][100];
+	char modificada[4][3][100];
+	int i, j;
+
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("Digite a string da posicao [%d][%d]: ", i, j);
+			scanf("%99s", original[i][j]);
+		}
+	}
+
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			strcpy(modificada[i][j], original[i][j]);
+		}
+	}
+
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			char *s = modificada[i][j];
+			int tamanho = (int)strlen(s);
+
+			if (tamanho == 0) {
+				continue;
+			}
+
+			char primeiro = s[0];
+			char ultimo = s[tamanho - 1];
+
+			if (ehVogal(primeiro) && ehConsoante(ultimo)) {
+				inverteString(s);
+			}
+		}
+	}
+
+	printf("\nMatriz Original:\n");
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%s ", original[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\nMatriz Modificada:\n");
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%s ", modificada[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
